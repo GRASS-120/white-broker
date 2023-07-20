73f5dc82727b674e99beee5ed7b301365efc8da4
GRASS_IT <45945921+GRASS-120@users.noreply.github.com>
2023-07-20 20:06:59 +0700

    скрипт с почтой заработал!

--- email-sendler/mail.js
+++ email-sendler/mail.js
@@ -15,27 +15,22 @@ class Mail {
 
     #getTransporter() {
         return nodemailer.createTransport({
-            //name: "nodemailer",
             host: EMAIL_HOST,
             port: EMAIL_PORT,
             secure: true,
             auth: {
-                // jrjfmsvcofbceolx
-                // clientId: "a5cd5fd2aa644c0087935449646832f6",
-                // clientSecret: "c31566f439bf4c468aa1e87e54ad6bd2",
                 user: EMAIL_HOST_USER,
                 pass: EMAIL_HOST_PASSWORD,
-                //accessToken: "be05f13c3895b9b10429d17c6a26ac30"
-                
-            },
+                // jparygojeujtlkti - пароль приложения
+            }
         });
     }
 
-    async send(reciever, message) {
+    async send(receiver, message) {
         try {
             const info = await this.#transporter.sendMail({
                 from: 'pokemon9116@yandex.ru',
-                to: reciever,
+                to: receiver,
                 subject: 'Welcome to Test site',
                 text: message,
                 html: `<b>${message}</b>`,
